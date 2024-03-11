8fab9df5b9c55797564d86d0fa4c49da4846ec93
subalakshmi-29 <subalakshmi161001@gmail.com>
2024-03-11 16:37:42 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://img.pikbest.com/png-images/blogging--man-blogger--flat-modern-vector-illustration_1318020.png!w700wp" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://blogging-application-backend-lilac.vercel.app"
